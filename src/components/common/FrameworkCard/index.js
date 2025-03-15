7032d8dbef1f9c4d8c3ff2804d6ba57cc61dad42
aegntic <aegntic@gmail.com>
2025-03-16 08:23:33 +1100

    Create index file for FrameworkCard component

---
+++ src/components/common/FrameworkCard/index.js
@@ -0,0 +1,9 @@
+/**
+ * FrameworkCard Index
+ * 
+ * Exports the FrameworkCard component and any related utilities.
+ */
+
+import FrameworkCard from './FrameworkCard';
+
+export default FrameworkCard;
