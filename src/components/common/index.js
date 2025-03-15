fd695015353b263646c4d96bd8bf669f1754e48e
aegntic <aegntic@gmail.com>
2025-03-16 08:22:04 +1100

    Create index file for common components

---
+++ src/components/common/index.js
@@ -0,0 +1,17 @@
+/**
+ * Common Components Index
+ * 
+ * This file exports all common components used across the IPP.TOOLS ecosystem.
+ * These components implement cross-cutting concerns and psychological engagement
+ * patterns.
+ */
+
+import FrameworkCard from './FrameworkCard';
+
+export {
+  FrameworkCard
+};
+
+export default {
+  FrameworkCard
+};
