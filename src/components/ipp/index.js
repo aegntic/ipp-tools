356487b1a22046c2f9a9839de930288167a0377e
aegntic <aegntic@gmail.com>
2025-03-16 08:22:12 +1100

    Create index file for IPP components

---
+++ src/components/ipp/index.js
@@ -0,0 +1,17 @@
+/**
+ * IPP.TOOLS Components Index
+ * 
+ * This file exports all components specific to the IPP.TOOLS main site.
+ * These components implement the psychological engagement framework for
+ * the IPP.TOOLS authority positioning ecosystem.
+ */
+
+import FrameworksSection from './FrameworksSection';
+
+export {
+  FrameworksSection
+};
+
+export default {
+  FrameworksSection
+};
