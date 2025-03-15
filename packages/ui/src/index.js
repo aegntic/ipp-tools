fe4f1a1e5a3b9e0616dc366fd59fc5026725a5d2
aegntic <aegntic@gmail.com>
2025-03-16 04:28:28 +1100

    Add UI package index file

---
+++ packages/ui/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * IPP.TOOLS UI Component Library
+ * 
+ * This module exports reusable UI components with built-in
+ * psychological optimization for maximum conversion efficiency.
+ */
+
+// Framework Components
+export { default as FrameworkCard } from './components/FrameworkCard/FrameworkCard';
+
+// Add other components here as they're implemented
+// export { default as WaitlistForm } from './components/WaitlistForm/WaitlistForm';
+// export { default as FeatureHighlight } from './components/FeatureHighlight/FeatureHighlight';
+// export { default as TestimonialCard } from './components/TestimonialCard/TestimonialCard';
